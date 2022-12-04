133b63cb7d1e259d6258caf0c73d38576e46ebc9
Rafael Pol Abellas <rafael.polabellas@boosterfuels.com>
2022-12-03 22:35:45 -0500

    day 2 WIP 😔

---
+++ src/2/1/index.ts
@@ -0,0 +1,61 @@
+import input from '../input'
+
+enum SHAPE {
+    ROCK = 'ROCK',
+    PAPER = 'PAPER',
+    SCISSOR = 'SCISSOR',
+}
+
+const WIN_MAP = {
+    [SHAPE.ROCK]: SHAPE.SCISSOR,
+    [SHAPE.PAPER]: SHAPE.ROCK,
+    [SHAPE.SCISSOR]: SHAPE.PAPER
+}
+
+type SHAPE_ALIAS_TYPE = {[key:string]: SHAPE}
+
+const SHAPE_ALIAS : SHAPE_ALIAS_TYPE = {
+    A: SHAPE.ROCK,
+    X: SHAPE.ROCK,
+    B: SHAPE.PAPER,
+    Y: SHAPE.PAPER,
+    C: SHAPE.SCISSOR,
+    Z: SHAPE.SCISSOR
+}
+
+const SHAPE_SCORE = {
+    [SHAPE.ROCK]: 1,
+    [SHAPE.PAPER]: 2,
+    [SHAPE.SCISSOR]: 3,
+}
+
+const OUTCOME_SCORE = {
+    LOSE: 0,
+    DRAW: 3,
+    WIN: 6
+}
+
+function translateMoves(moves : string[]) {
+    return moves.map((move: string) => SHAPE_ALIAS[move])
+}
+
+export default function main() {
+    const inputArray = input.split(/\n/)
+    let totalScore = 0;
+    for(const play of inputArray) {
+        const [oppMove, yourMove] = translateMoves(play.split(' '))
+        console.log({oppMove, yourMove})
+        if(WIN_MAP[oppMove] === yourMove) {
+            console.log('lose!')
+            totalScore += OUTCOME_SCORE.LOSE
+        }
+        else if (oppMove === yourMove) {
+            console.log('draw')
+            totalScore += OUTCOME_SCORE.DRAW + SHAPE_SCORE[yourMove]
+        } else {
+            console.log('win')
+            totalScore += OUTCOME_SCORE.WIN + SHAPE_SCORE[yourMove]
+        }
+    }
+    return totalScore
+}
